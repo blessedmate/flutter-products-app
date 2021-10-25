5d419c91831271fb42e4a857398e3721990795a4
Mate <md.quintero@uniandes.edu.co>
2021-10-25 12:15:32 -0500

    init

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_products_app/screens/screens.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Products App',
+      initialRoute: 'login',
+      routes: {
+        'login': (_) => LoginScreen(),
+        'home': (_) => HomeScreen(),
+      },
+    );
+  }
+}
---
+++ lib/screens/screens.dart
@@ -0,0 +1,2 @@
+export 'package:flutter_products_app/screens/home_screen.dart';
+export 'package:flutter_products_app/screens/login_screen.dart';
