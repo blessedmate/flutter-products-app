9e00b503828ef95a49ae10b64cfcd7ee19fe8412
Mate <md.quintero@uniandes.edu.co>
2021-10-26 22:27:15 -0500

    login inputs visible

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_products_app/ui/custom_input_decorations.dart';
 import 'package:flutter_products_app/widgets/widgets.dart';
 
 class LoginScreen extends StatelessWidget {
@@ -37,11 +38,69 @@ class _LogInBox extends StatelessWidget {
                   style: Theme.of(context).textTheme.headline4,
                 ),
                 const SizedBox(height: 30),
-                const Text('Form')
+                const _LoginForm(),
               ],
             ),
           ),
+          const SizedBox(height: 50),
+          const Text('Create a new account', style: TextStyle(fontSize: 18)),
         ],
+      ),
+    );
+  }
+}
+
+class _LoginForm extends StatelessWidget {
+  const _LoginForm({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Form(
+        //TODO: Reference to key
+        child: Column(
+          children: [
+            TextFormField(
+              autocorrect: false,
+              keyboardType: TextInputType.emailAddress,
+              decoration: CustomInputDecorations.loginInputDecoration(
+                hintText: 'example@mail.com',
+                labelText: 'Email',
+                prefixIcon: Icons.alternate_email,
+              ),
+            ),
+            const SizedBox(height: 30),
+            TextFormField(
+              autocorrect: false,
+              obscureText: true,
+              keyboardType: TextInputType.emailAddress,
+              decoration: CustomInputDecorations.loginInputDecoration(
+                hintText: '********',
+                labelText: 'Password',
+                prefixIcon: Icons.lock_outline,
+              ),
+            ),
+            const SizedBox(height: 30),
+            MaterialButton(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10)),
+              disabledColor: Colors.grey,
+              elevation: 0,
+              color: Colors.deepPurple,
+              child: Container(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 80, vertical: 15),
+                child: const Text(
+                  'Sign in',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+              onPressed: () {
+                // TODO: Login form
+              },
+            )
+          ],
+        ),
       ),
     );
   }
