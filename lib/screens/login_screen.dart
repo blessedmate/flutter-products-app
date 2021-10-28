70a91915a6cfa5cdb5583ba350de2990dd05dc04
Mate <md.quintero@uniandes.edu.co>
2021-10-27 23:17:57 -0500

    full login validation and provider state manager

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_products_app/providers/login_form_provider.dart';
 import 'package:flutter_products_app/ui/custom_input_decorations.dart';
 import 'package:flutter_products_app/widgets/widgets.dart';
+import 'package:provider/provider.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({Key? key}) : super(key: key);
@@ -38,7 +40,13 @@ class _LogInBox extends StatelessWidget {
                   style: Theme.of(context).textTheme.headline4,
                 ),
                 const SizedBox(height: 30),
-                const _LoginForm(),
+
+                // Wrap the widget that will be updated in a ChangeNotifierProvider for state management.
+
+                ChangeNotifierProvider(
+                  create: (_) => LoginFormProvider(),
+                  child: const _LoginForm(),
+                )
               ],
             ),
           ),
@@ -55,11 +63,16 @@ class _LoginForm extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // Reference to provider
+    final loginForm = Provider.of<LoginFormProvider>(context);
     return Container(
       child: Form(
-        //TODO: Reference to key
+        // Reference to provider key
+        key: loginForm.formKey,
+        autovalidateMode: AutovalidateMode.onUserInteraction,
         child: Column(
           children: [
+            // Email Form Field
             TextFormField(
               autocorrect: false,
               keyboardType: TextInputType.emailAddress,
@@ -68,8 +81,18 @@ class _LoginForm extends StatelessWidget {
                 labelText: 'Email',
                 prefixIcon: Icons.alternate_email,
               ),
+              onChanged: (value) => loginForm.email = value,
+              // Email Validator using regular expression
+              validator: (value) {
+                String pattern =
+                    r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+                RegExp regExp = RegExp(pattern);
+                return regExp.hasMatch(value ?? '') ? null : 'Email not valid';
+              },
             ),
             const SizedBox(height: 30),
+
+            // Password Form Field
             TextFormField(
               autocorrect: false,
               obscureText: true,
@@ -79,6 +102,13 @@ class _LoginForm extends StatelessWidget {
                 labelText: 'Password',
                 prefixIcon: Icons.lock_outline,
               ),
+              onChanged: (value) => loginForm.password = value,
+              validator: (value) {
+                if (value != null && value.length >= 6) {
+                  return null;
+                }
+                return 'Must be at least 6 characters long';
+              },
             ),
             const SizedBox(height: 30),
             MaterialButton(
@@ -90,14 +120,29 @@ class _LoginForm extends StatelessWidget {
               child: Container(
                 padding:
                     const EdgeInsets.symmetric(horizontal: 80, vertical: 15),
-                child: const Text(
-                  'Sign in',
+                child: Text(
+                  loginForm.isLoading ? '...' : 'Sign in',
                   style: TextStyle(color: Colors.white),
                 ),
               ),
-              onPressed: () {
-                // TODO: Login form
-              },
+              onPressed: loginForm.isLoading
+                  ? null
+                  : () async {
+                      FocusScope.of(context).unfocus();
+
+                      if (!loginForm.isValidForm()) {
+                        return;
+                      } else {
+                        loginForm.isLoading = true;
+
+                        await Future.delayed(Duration(seconds: 2));
+
+                        // TODO: Login validation
+                        loginForm.isLoading = false;
+
+                        Navigator.pushReplacementNamed(context, 'home');
+                      }
+                    },
             )
           ],
         ),
